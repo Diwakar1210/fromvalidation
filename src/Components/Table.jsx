20a8bd315be77bddd09f9ef9520566b8393843dd
Diwakar1210 <diwakar.b1999@gmail.com>
2025-08-04 16:15:07 +0530

    Working on Table edit and view data

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -8,7 +8,15 @@ const Table = () => {
             .then(res => res.json())
             .then(data => setData(data));
     }, []);
-    console.log(data);
+
+    function handleEdit(id){
+        console.log(id);
+    }
+
+    function handleDelete(id){
+        setData((data)=> data.filter((item)=>item.id !== id));
+    }
+
     return (
         <>
             <table className='w-full border-collapse border border-gray-400'>
@@ -24,7 +32,7 @@ const Table = () => {
                 </tbody>
                 <tbody>
                 {
-                    data.map((item) => (
+                    data.length > 0 ? data.map((item) => (
                             <tr key={item.id}>
                                 <td className='border border-gray-400 text-center p-2'>{item.id}</td>
                                 <td className='border border-gray-400 text-center'>{item.name}</td>
@@ -33,12 +41,12 @@ const Table = () => {
                                 <td className='border border-gray-400 text-center'>{item.phone}</td>
                                 <td className='border border-gray-400 text-center'>
                                     <button className='cursor-pointer'>👁️</button>
-                                    <button className='cursor-pointer'>✏️</button>
-                                    <button className='cursor-pointer'>⚰️</button>
+                                    <button className='cursor-pointer' onClick={()=>handleEdit(item.id)}>✏️</button>
+                                    <button className='cursor-pointer' onClick={()=>handleDelete(item.id)}>⚰️</button>
                                 </td>
                             </tr>
                         )
-                    )
+                    ) : <tr><td colSpan='6' className='text-center p-2'>No Data Available</td></tr>
                 }
                 </tbody>
             </table>
--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -5,7 +5,10 @@ const User = () => {
     return (
         <>
             <div className='p-4'>
-                <h1 className='mb-3 text-2xl'>Table</h1>
+                <div className='flex items-center justify-between mb-3'>
+                    <h1 className='text-2xl'>Table</h1>
+                    <button className='bg-sky-500 rounded py-2 px-3.5 text-sm'>Add User</button>
+                </div>
                 <Table/>
             </div>
         </>
